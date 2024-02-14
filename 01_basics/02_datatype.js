e9982ce98f1a9f3491a9cbd341c8c7ddbc5f7017
DevTusharSRG <142409048+DevTusharSRG@users.noreply.github.com>
2024-02-14 17:55:36 +0000

    datatype in js defination

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,22 @@
+/*
+types of datatype
+number = represents the numerical value 
+string = represents the bundle of chars
+bigint = represents yhe numerical data
+null = represents the null value
+undefined = the value does not defined to variable
+symbol 
+boolean = represents the true or false value
+*/
+let name = "Satish"
+let age = 20
+let contactNo = BigInt(1234567890)
+let job = true
+let college;
+let location = null
+console.log("type of name=",typeof name)
+console.log("type of age=",typeof age)
+console.log("type of contactno=",typeof contactNo)
+console.log("type of job=",typeof job)
+console.log("type of college=",typeof college)
+console.log("type of location=",typeof location)
