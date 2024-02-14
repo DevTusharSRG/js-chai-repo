56a4b1fec2fcff02d399a669f85dd26645ab6719
DevTusharSRG <142409048+DevTusharSRG@users.noreply.github.com>
2024-02-14 17:03:17 +0000

    variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId =123456
+let accountEmail = "srg@mail.com"
+var accountPassword = "1234"
+accountCity = "Pune"
+let accountState 
+
+//because of const is an constatnt value it cannot define again hence the accountId is constant it could not change
+
+accountEmail = "srg1@mail.com"
+accountPassword = "0987"
+accountCity = "mumbai"
+ 
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountState,accountCity])
