3f83e0645fcefc9496340fbd55e05d2ef43b9ecd
DevTusharSRG <142409048+DevTusharSRG@users.noreply.github.com>
2024-02-14 18:11:15 +0000

    conversion basics

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+let value = "33"
+console.log(typeof value)
+let numberConversion = Number(value)
+console.log(numberConversion)
+/*
+if the given value is number and convert into number then it convert sucessfully
+if the value is string then "33abc" => NaN 
+if the value is boolean then true => 1
+if the value is null then null => 0
+if the value is undefine then undefined => NaN
+*/
+let stringVal = 123
+console.log(typeof stringVal)
+let stringConversion = String(stringVal)
+console.log(stringConversion)
