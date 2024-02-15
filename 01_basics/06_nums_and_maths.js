909654ee1e1639dc5caa45c7ea304600a036d21f
DevTusharSRG <142409048+DevTusharSRG@users.noreply.github.com>
2024-02-15 18:16:04 +0000

    number and math function

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,14 @@
+let age = 23                    //it defines the number 
+const number = new Number(123000)   //it defines the given variable is number
+
+console.log(number);
+console.log(number.toFixed(2));
+console.log(number.toLocaleString());
+
+
+
+///////////////////////////////////////////////
+//maths function
+console.log(Math.random());
+console.log(Math.round(123.56));
+console.log(Math.sqrt(9));
