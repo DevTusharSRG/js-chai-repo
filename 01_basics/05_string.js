87679b16cc1f58fb8e6ff7f7bc33960e91cd7fed
DevTusharSRG <142409048+DevTusharSRG@users.noreply.github.com>
2024-02-15 17:59:52 +0000

    String basics

---
+++ 01_basics/05_string.js
@@ -0,0 +1,14 @@
+ const name = "DevTushar"        //this is simple term to define the string
+// console.log(name);
+ const myString = new String ("Tushar")      //it defines the proper string type variable
+// console.log(myString);
+
+//older vertsion to concatination of string
+console.log("my github name is "+name+" and may name is "+myString);
+
+//modern type to define the concatination
+console.log(`My Github Account name is ${name} and my Name is ${myString}`)
+
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.length)
