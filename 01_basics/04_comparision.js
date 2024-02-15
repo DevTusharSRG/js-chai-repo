53bdf3f9beca5bd07ad2c95f677660efdc99bc3d
DevTusharSRG <142409048+DevTusharSRG@users.noreply.github.com>
2024-02-15 04:23:21 +0000

    comparision with js

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+console.log(1>2)
+console.log(1<2)
+console.log(1==2)
+console.log(1!=2);
+console.log("9"==9);
+console.log("9"===9);
+console.log(5>=5);
+console.log(5<=5);
+
+
+//checking the datatype null value with comparision
+console.log(null>0);
+console.log(null<0);
+console.log(null==0);
+console.log(null<=1);
+
+//checking the datatype undefined value with comparision
+console.log(undefined>0);
+console.log(undefined<1)
+console.log(undefined==1);
