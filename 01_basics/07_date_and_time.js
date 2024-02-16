fb4c72c2830e13d7f3f19ebf1c8d4f8a7e33d086
DevTusharSRG <142409048+DevTusharSRG@users.noreply.github.com>
2024-02-16 02:51:34 +0000

    date and time

---
+++ 01_basics/07_date_and_time.js
@@ -0,0 +1,14 @@
+//Date
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+
+let myCreatedDate = new Date(2024 , 0 ,13 ,5,55)
+console.log(myCreatedDate);
+
+console.log(myCreatedDate.getDate());
+console.log(myCreatedDate.getDay());
+console.log(myCreatedDate.getFullYear());
+console.log(myCreatedDate.getMonth());
+console.log(myCreatedDate.getTime());
