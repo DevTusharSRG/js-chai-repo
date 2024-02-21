74c77288828ca57f80ba15ea1b4bd984483b8f1b
DevTusharSRG <142409048+DevTusharSRG@users.noreply.github.com>
2024-02-21 13:35:06 +0000

    while loop

---
+++ 05_iterators/05_while.js
@@ -0,0 +1,19 @@
+//while loop
+
+// while (condition) {
+    
+// }
+let i=1
+while (i<=10) {
+   console.log(i);
+   i++ 
+}
+
+// do {
+    
+// } while (condition);
+
+do {
+    console.log(i);
+    i++
+} while (i<20);
