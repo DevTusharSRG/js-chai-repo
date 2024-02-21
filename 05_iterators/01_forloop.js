220f4ef5b373ff9bab41c151ed58fb8af6d66a43
DevTusharSRG <142409048+DevTusharSRG@users.noreply.github.com>
2024-02-21 13:28:08 +0000

    simple for loop

---
+++ 05_iterators/01_forloop.js
@@ -0,0 +1,20 @@
+// for (let index = 0; index < 10; index++) {
+//     const element =index;
+//     console.log(element);
+// }
+
+//loop in loop
+
+// for(i=1 ;i<=10;i++){
+//     console.log(`table of ${i}`);
+//     for(j=1;j<=10;j++){
+//         console.log(i+"*"+j+"="+i*j);
+//     }
+// }
+
+const heros =["ironman", "batman", "superman"]
+
+for (let index = 0; index < heros.length; index++) {
+    const element = heros[index];
+    console.log(element);
+}
